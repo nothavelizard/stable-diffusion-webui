2d1af81a9bcd66ad35c5d67d6d0d294ddbd352d2
nothavelizard <djodonohue@gmail.com>
2023-05-15 09:36:23 -0500

    e

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--api --cors-allow-origins=* --share
 
 call webui.bat
--- webui-user.sh
+++ webui-user.sh
@@ -10,10 +10,10 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+# export COMMANDLINE_ARGS="--api --cors-allow-origins=* --share"
 
 # python3 executable
-#python_cmd="python3"
+python_cmd="python"
 
 # git executable
 #export GIT="git"
